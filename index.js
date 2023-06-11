5f7d0b4fb92e1308e4d4c9559d8c3c21f45c3e55
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-06-11 22:35:17 +0600

    jwt added

--- index.js
+++ index.js
@@ -9,6 +9,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1fwa7gk.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -34,10 +52,15 @@ async function run() {
     const usersCollection = client.db('enigmaDB').collection("users")
 
 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+      res.send({ token })
+    })
 
     // users related apis
-
-    app.get('/users', async(req,res)=>{
+    app.get('/users',verifyJWT, async(req,res)=>{
       const result = await usersCollection.find().toArray();
       res.send(result);
     })
@@ -53,7 +76,7 @@ async function run() {
       res.send(result);
     })
 
-    app.patch('/users/admin/:id', async(req,res)=>{
+    app.patch('/users/admin/:id',verifyJWT, async(req,res)=>{
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
       const updateDoc = {
@@ -65,7 +88,7 @@ async function run() {
       res.send(result);
     })
 
-    app.patch('/users/instructor/:id', async(req,res)=>{
+    app.patch('/users/instructor/:id',verifyJWT, async(req,res)=>{
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)};
       const updateDoc = {
@@ -85,7 +108,7 @@ async function run() {
     });
 
     // my selected class collection
-    app.get("/mySelectedClass", async (req, res) => {
+    app.get("/mySelectedClass", verifyJWT, async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
@@ -109,7 +132,7 @@ async function run() {
     });
 
     // create payment intent
-    app.post("/create-payment-intent", async (req, res) => {
+    app.post("/create-payment-intent", verifyJWT, async (req, res) => {
       const { price } = req.body;
       const amount = price * 100;
       const paymentIntent = await stripe.paymentIntents.create({
@@ -123,12 +146,12 @@ async function run() {
     });
 
     // payment related api
-    app.get('/payments', async (req, res) => {
+    app.get('/payments',  async (req, res) => {
       const result = await paymentCollection.find().sort({ date: -1 }).toArray();
       res.send(result);
     });
     
-    app.post('/payments', async(req,res)=>{
+    app.post('/payments', verifyJWT, async(req,res)=>{
       const payment = req.body;
       const insertResult = await paymentCollection.insertOne(payment);
 
