2f04125aafa37849e3947fc5147de179a3b59c50
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-06-11 15:37:16 +0600

    payment secret key added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 // middleware
