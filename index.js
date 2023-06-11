2175d3f35d5b830476e05f9dc2a355f642a007c9
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-06-11 22:03:51 +0600

    users admin patch added

--- index.js
+++ index.js
@@ -36,9 +36,32 @@ async function run() {
 
 
     // users related apis
+
+    app.get('/users', async(req,res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
     app.post('/users', async(req,res)=>{
       const user = req.body;
+      const query = {email: user.email};
+      const existingUser = await usersCollection.findOne(query)
+      if(existingUser) {
+        return res.send({message: 'user already exists'})
+      }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc)
       res.send(result);
     })
 
