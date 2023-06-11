f7c74e59c43af685d9ab0f456610d9185cfe24bf
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-06-12 01:32:41 +0600

    await remove from client

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     client.connect();
 
     const classesCollection = client.db("enigmaDB").collection("classes");
     const mySelectedClassCollection = client
