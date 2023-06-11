94c2d4fa53939131476885746eb7e22d3a9d9732
Alif Chowdhury <chowdhuryalif42@gmail.com>
2023-06-11 15:43:40 +0600

    payment intent created

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
-const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
+const cors = require("cors");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1fwa7gk.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -19,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -27,47 +26,60 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
-
-    const classesCollection = client.db("enigmaDB").collection('classes');
-    const mySelectedClassCollection = client.db("enigmaDB").collection('mySelectedClass');
-
+    const classesCollection = client.db("enigmaDB").collection("classes");
+    const mySelectedClassCollection = client
+      .db("enigmaDB")
+      .collection("mySelectedClass");
 
     // classes
-    app.get('/classes', async(req,res)=>{
-        const result = await classesCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/classes", async (req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    });
 
     // my selected class collection
-    app.get('/mySelectedClass', async(req,res)=>{
-        const email = req.query.email;
-        if(!email) {
-            res.send([])
-        }
-        const query = {email: email};
-        const result= await mySelectedClassCollection.find(query).toArray();
-        res.send(result);   
-    })
+    app.get("/mySelectedClass", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await mySelectedClassCollection.find(query).toArray();
+      res.send(result);
+    });
 
-    app.post('/mySelectedClass', async(req,res)=>{
-        const item = req.body;
-        const result = await mySelectedClassCollection.insertOne(item);
-        res.send(result);
-    })
+    app.post("/mySelectedClass", async (req, res) => {
+      const item = req.body;
+      const result = await mySelectedClassCollection.insertOne(item);
+      res.send(result);
+    });
 
+    app.delete("/mySelectedClass/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await mySelectedClassCollection.deleteOne(query);
+      res.send(result);
+    });
 
-    app.delete('/mySelectedClass/:id', async(req,res)=>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
-        const result = await mySelectedClassCollection.deleteOne(query);
-        res.send(result);
-    })
-
+    // create payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -75,11 +87,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('enigma magic server is running')
-})
+app.get("/", (req, res) => {
+  res.send("enigma magic server is running");
+});
 
 app.listen(port, () => {
   console.log(`Enigma magic server is running on port ${port}`);
-})
+});
